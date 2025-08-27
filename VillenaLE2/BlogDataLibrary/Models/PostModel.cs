328a42073e48fe289c3a5514a88799c0866315b1
xyzavx <xyzavillena@gmail.com>
2025-08-27 10:40:27 +0800

    Add LE2

---
+++ VillenaLE2/BlogDataLibrary/Models/PostModel.cs
@@ -0,0 +1,8 @@
+﻿public class PostModel
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public DateTime DateCreated { get; set; }
+}
