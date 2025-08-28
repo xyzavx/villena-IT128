a7326373c7c6a822857ec955040733cb653d8d15
xyzavx <xyzavillena@gmail.com>
2025-08-28 16:54:17 +0800

    Add

---
+++ VillenaLE2/BlogWeb/Program.cs
@@ -0,0 +1,26 @@
+using BlogDataLibrary.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Dependency Injection
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+
+var app = builder.Build();
+
+// Configure middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
