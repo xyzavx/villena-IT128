2247f68a3d06b3cbc4e72ed09907b2424cb31402
xyzavx <xyzavillena@gmail.com>
2025-08-28 16:57:26 +0800

    Add files via upload

---
+++ VillenaLE2/BlogWeb/Controllers/AuthControl.cs
@@ -0,0 +1,32 @@
+﻿using BlogDataLibrary.Database;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly ISqlDataAccess _db;
+
+    public AuthController(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    [HttpPost("register")]
+    public void Register([FromBody] UserModel user)
+    {
+        string sql = "INSERT INTO Users (UserName, FirstName, LastName, Password) VALUES (@UserName, @FirstName, @LastName, @Password)";
+        _db.SaveData(sql, user, "SqlDb", false);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] UserModel login)
+    {
+        string sql = "SELECT * FROM Users WHERE UserName = @UserName AND Password = @Password";
+        var users = _db.LoadData<UserModel, UserModel>(sql, login, "SqlDb", false);
+        if (users.Count > 0)
+            return Ok(users[0]);
+        else
+            return Unauthorized();
+    }
+}
---
+++ VillenaLE2/BlogWeb/Controllers/PostControl.cs
@@ -0,0 +1,30 @@
+﻿using BlogDataLibrary.Database;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PostsController : ControllerBase
+{
+    private readonly ISqlDataAccess _db;
+
+    public PostsController(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<ListPostModel>> GetAllPosts()
+    {
+        string sql = "SELECT p.Id, p.Title, p.Body, p.DateCreated, u.UserName, u.FirstName, u.LastName " +
+                     "FROM Posts p INNER JOIN Users u ON p.UserId = u.Id";
+
+        return _db.LoadData<ListPostModel, dynamic>(sql, new { }, "SqlDb", false);
+    }
+
+    [HttpPost]
+    public void AddPost([FromBody] PostModel post)
+    {
+        string sql = "INSERT INTO Posts (UserId, Title, Body, DateCreated) VALUES (@UserId, @Title, @Body, @DateCreated)";
+        _db.SaveData(sql, post, "SqlDb", false);
+    }
+}
