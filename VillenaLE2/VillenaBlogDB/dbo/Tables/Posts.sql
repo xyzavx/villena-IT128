afa1b2ed76431625a8f292988d7b4481659f1c3a
xyzavx <xyzavillena@gmail.com>
2025-08-28 16:43:52 +0800

    Delete VillenaLE2 directory

--- VillenaLE2/VillenaBlogDB/dbo/Tables/Posts.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Posts] 
-(
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
-    [UserId] INT NOT NULL,
-    [Title] NVARCHAR(150) NOT NULL,
-    [Body] TEXT NOT NULL,
-    [DateCreated] DATETIME2 NOT NULL,
-    CONSTRAINT [FK_Posts_Users] FOREIGN KEY (UserId) REFERENCES Users(Id)
-);
--- VillenaLE2/VillenaBlogDB/dbo/Tables/Users.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Users]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [UserName] NVARCHAR(16) NOT NULL, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [Password] NVARCHAR(16) NOT NULL
-)
